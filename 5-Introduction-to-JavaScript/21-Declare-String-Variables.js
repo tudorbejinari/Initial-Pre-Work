62527affb02d45fd3a6e3b8e35ae57b05d231880
Tudor Bejinari <tudorbejinari@gmail.com>
2019-06-23 13:57:01 -0600

    Added solution and commented what I learned...

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,6 @@
+/* In this example I create 2 string variables and I assign them values of my first and last name . Dont forget when you name your variables to use camelCase and strings should be enclosed in quotes..*/
+// Example
+var myFirstName = "Tudor";
+var myLastName = "Bejinari";
+
+// Only change code below this line
---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+// Escaping Literal Quotes in Strings by placing a backsplash (\) in front of the quote. see the example below:
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Changed
